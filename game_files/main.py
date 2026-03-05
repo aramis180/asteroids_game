0b400702ce4e37cbdb5a770dbd0bb3a4b752f6bb
aramis180 <alekm180@gmail.com>
2026-03-05 20:36:06 +0100

    Created directory with game files and file that will store constant values

---
+++ game_files/main.py
@@ -0,0 +1,14 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    print(
+        f"Starting Asteroids with pygame version: {pygame.version.ver}\n"
+        f"Screen width: {SCREEN_WIDTH}\n"
+        f"Screen height: {SCREEN_HEIGHT}"
+    )
+
+
+if __name__ == "__main__":
+    main()
